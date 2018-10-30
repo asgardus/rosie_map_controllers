a379bb5b1527fbd89673280c5b746e7cc26b700d
TNeverWood <tobiasniewalda@web.de>
2018-10-30 14:28:12 +0100

    Wrote reader code in the rosie_map_provider

--- src/rosie_map_provider.cpp
+++ src/rosie_map_provider.cpp
@@ -11,6 +11,7 @@
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/Point.h>
 #include <string>
+#include <sstream>
 #include <math.h>
 #include <iostream>
 #include <pcl_ros/transforms.h>
@@ -22,61 +23,148 @@ float pi = 3.14159265359;
 float resolution;
 int width = 1;
 int height = 1;
-std_msgs::Int8 *occGrid;
+
+float robotsize = 0.2;
+static std_msgs::Int8 *occGrid;
+visualization_msgs::Marker *markers;
+
+static ros::Subscriber map_sub;
+static ros::Publisher grid_publisher;
+
 
 ros::Time load_time;
 
-void initializeMap(){
-	width = 300;
-	height = 300;
+char mapInitialized = 0;
+void initializeMap(const visualization_msgs::MarkerArray msg){	
+	ROS_INFO("Initializing!");
+	
+	map_sub = ros::Subscriber();
+
+	width = 480;
+	height = 480;
 	resolution = 0.01f;
-	occGrid = (std_msgs::Int8*)malloc(sizeof(std_msgs::Int8)*width*height);
-	for(int i = 0; i < height; ++i){
-		for(int j = 0; j < width; ++j){
-			if(i == 0){
-				occGrid[i*height+j].data = 150;
-			}else if(i == height-1){
-				occGrid[i*height+j].data = 150;				
-			}else if(j == 0){
-				occGrid[i*height+j].data = 150;
-			}else if(j == width-1){
-				occGrid[i*height+j].data = 150;			
-			}else if(i == 100 && j < 200){
-				occGrid[i*height+j].data = 150;
-			}else{
-				occGrid[i*height+j].data = 0;
+	int numbMarkers = msg.markers.size();
+	//markers = (visualization_msgs::Marker*)malloc(sizeof(visualization_msgs::Marker)*numbMarkers);
+	//markers = msg.markers;
+	
+	int czone = robotsize/((float)2*resolution) + 0.02/resolution; //additional extra security distance
+
+	float* pointArray = (float*)malloc(sizeof(float)*4*numbMarkers);
+	for(int i = 0; i < 4*numbMarkers; ++i){
+		pointArray[i] = 0.0f;	
+	}
+
+	float minX, minY, maxX, maxY;
+	minX = minY = maxX = maxY = 0;
+	for(int k = 0; k < numbMarkers; ++k){
+		//Extract point data
+		std::string pointsText = msg.markers[k].text;
+		std::stringstream ss(pointsText);
+		float sX, sY, eX, eY;
+		ss>>sX;
+		ss>>sY;
+		ss>>eX;
+		ss>>eY;
+
+		//Set point data on every 4th index
+		pointArray[k<<2]=sX;
+		pointArray[(k<<2)+1]=sY;
+		pointArray[(k<<2)+2]=eX;
+		pointArray[(k<<2)+3]=eY;
+
+		if(sX < minX){
+			minX = sX;
+		}
+		if(sX > maxX){
+			maxX = sX;
+		}
+		if(eX < minX){
+			minX = eX;
+		}
+		if(eX > maxX){
+			maxX = eX;
+		}
+		if(sY < minY){
+			minY = sY;
+		}
+		if(sY > maxY){
+			maxY = sY;
+		}
+		if(eY < minY){
+			minY = eY;
+		}
+		if(eY > maxY){
+			maxY = eY;
+		}
+		ROS_INFO("sX: %f, sY: %f, eX: %f, eY: %f", sX, sY, eX, eY);
+	}
+
+	float offsetX = minX;
+	float offsetY = minY;
+
+	width = (maxX - minX)/resolution;
+	height = (maxY - minY)/resolution;
+
+	ROS_INFO("minX: %f, minY: %f, maxX: %f, maxY: %f, offsetX: %f, offsetY: %f", minX, minY, maxX, maxY, offsetX, offsetY);
+	ROS_INFO("width: %d, height: %d", width, height);
+
+	occGrid = (std_msgs::Int8*)malloc(sizeof(std_msgs::Int8)*width*height); //Int8 representation, no matrix
+	for(int o = 0; o < width*height; ++o){
+		occGrid[o].data = 0;
+	}
+	for(int k = 0; k < numbMarkers; ++k){
+		float x1 = (pointArray[k<<2]) - offsetX;
+		float y1 = (pointArray[(k<<2)+1]) - offsetY;
+		float x2 = (pointArray[(k<<2)+2]) - offsetX;
+		float y2 = (pointArray[(k<<2)+3]) - offsetY;
+
+		float diffX = (x2-x1);
+		float diffY = (y2-y1);
+
+		float wallDist = sqrt((diffY*diffY)+(diffX*diffX));
+		if(wallDist == 0){
+			continue;
+		}
+
+		for(float d = 0.0; d <= wallDist; d+=resolution){
+			int px = (int)((d*((x2-x1)/wallDist)+x1)/resolution);
+			int py = (int)((d*((y2-y1)/wallDist)+y1)/resolution);
+
+			ROS_INFO("setting cell occupancy, x: %d, y: %d, d: %f, wallDist: %f, diffX: %f, diffY: %f", px, py, d, wallDist, diffX, diffY);
+
+			for(int y = -czone; y <= czone; y++){
+				for(int x = -czone; x <= czone; x++){
+					if((px+x) >= 0 && (px+x) < width && (py+y) >= 0 && (py+y) < height){
+						occGrid[(py+y)*width+px+x].data = 125;
+					}
+				}
 			}
 		}
 	}
-}
-
-void mapUpdateCallback(){
-	
+	ROS_INFO("occGrid set!");
+	mapInitialized = 1;
 }
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "rosie_map_provider");
 
     ros::NodeHandle n;
- 
-    ros::Publisher grid_publisher = n.advertise<nav_msgs::OccupancyGrid>("/rosie_occupancy_grid",1);
+    map_sub = n.subscribe<visualization_msgs::MarkerArray>("/maze_map", 1, initializeMap); 
+    grid_publisher = n.advertise<nav_msgs::OccupancyGrid>("/rosie_occupancy_grid",1);
 
     ros::Rate loop_rate(5);
- 
-	tf::Transform transform;
-    static tf::TransformBroadcaster br;
-    transform.setOrigin( tf::Vector3(0, 0, 0) );
-    tf::Quaternion qtf;
-    qtf.setRPY(0, 0, 0);
-    transform.setRotation( qtf );
-    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "map"));
 
-	initializeMap();
+	static tf::TransformBroadcaster br;
 
 	load_time = ros::Time::now();
 	int seq = 0;
-    while(ros::ok()){
-    	nav_msgs::OccupancyGrid mapgrid;
+	while(ros::ok()){
+		if(!mapInitialized){
+		    ros::spinOnce();
+	    	loop_rate.sleep();
+			continue;
+		}
+	    nav_msgs::OccupancyGrid mapgrid;
 
 		mapgrid.header.seq = seq++;
 		mapgrid.header.stamp = ros::Time::now();
@@ -92,18 +180,26 @@ int main(int argc, char **argv){
 
 		mapgrid.info.map_load_time = load_time;
 		mapgrid.info.resolution = resolution;
+
 		mapgrid.info.width = width;
 		mapgrid.info.height = height;
-
 		mapgrid.data.clear();
-
-		//mapgrid.data = (std_msgs::Int8*)malloc(sizeof(std_msgs::Int8)*width*height);
 		for(int i = 0; i < width*height; ++i){
 			mapgrid.data.push_back(occGrid[i].data);
 		}
 
-	    grid_publisher.publish(mapgrid);			
+	    grid_publisher.publish(mapgrid);
+
+		tf::Transform transform;
+		transform.setOrigin( tf::Vector3(0, 0, 0) );
+		tf::Quaternion qtf;
+		qtf.setRPY(0, 0, 0);
+		transform.setRotation( qtf );
+		br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "map"));			
+
 	    ros::spinOnce();
+
     	loop_rate.sleep();
+
     }
 }
